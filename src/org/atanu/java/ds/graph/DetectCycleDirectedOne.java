1615a62f4eb26f081da9d2bae1274f9d4df03937
Atanu Mondal <atamonda@oradev.oraclecorp.com>
2020-09-03 23:51:37 +0530

    Graph Problem added

---
+++ src/org/atanu/java/ds/graph/DetectCycleDirectedOne.java
@@ -0,0 +1,54 @@
+package org.atanu.java.ds.graph;
+
+public class DetectCycleDirectedOne {
+
+    private static boolean hasCycle(Graph graph, int v, boolean[] discovered, boolean[] inResurssion) {
+
+        //If it is in recurssion return True
+        if (inResurssion[v] == true) {
+            return true;
+        }
+        
+        //Mark in recursion
+        inResurssion[v] = true;
+        //Mark it visited. We can mark after the while as well.
+        discovered[v] = true;
+        for (int u : graph.adjList.get(v)) {
+            if (hasCycle(graph, u, discovered, inResurssion)) {
+                return true;
+            }
+
+        }
+
+        // Make the recurssion is false again
+        inResurssion[v] = false;
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        int N = 4;
+        boolean[] discovered = new boolean[N];
+        boolean[] inResurssion = new boolean[N];
+
+        Graph graph = new Graph(N);
+        graph.addEdge(new Edge(0, 1), true);
+        graph.addEdge(new Edge(0, 2), true);
+        graph.addEdge(new Edge(1, 2), true);
+        graph.addEdge(new Edge(2, 0), true);
+        graph.addEdge(new Edge(2, 3), true);
+        graph.addEdge(new Edge(3,3),true);
+
+        for (int i = 0; i < N; i++) {
+
+           if(!discovered[i] && hasCycle(graph, i, discovered, inResurssion)){
+                System.out.println("This Graph Has Cycle"); // Will Print True for each connected component. you can return here
+           }
+            if (!discovered[i]) {
+                boolean result = hasCycle(graph, i, discovered, inResurssion);
+                System.out.println(result + " " + i); // Printing the cycle check for non conncted components
+            }
+        }
+    }
+}
--- src/org/atanu/java/ds/graph/Graph.java
+++ src/org/atanu/java/ds/graph/Graph.java
@@ -9,15 +9,26 @@ public class Graph {
 	// A List of Lists to represent an adjacency list
 	List<LinkedList<Integer>> adjList = null;
 
-	public Graph(List<Edge> edges , int N) {
+	public Graph(int N) {
+		createEpmtyEdges(N);
+	}
 
-		adjList = new ArrayList<>(N);
+	public Graph(List<Edge> edges, int N) {
 
-		for(int i = 0; i < N ; i++) {
-			adjList.add(i, new LinkedList<Integer>());
-		}
+		createEpmtyEdges(N);
+		addEdges(edges, false);
+	}
 
-		for(int i = 0; i < edges.size() ; i++) {
+	public Graph(List<Edge> edges, int N, boolean Directed) {
+
+		createEpmtyEdges(N);
+		addEdges(edges, true);
+
+	}
+
+	public void addEdges(List<Edge> edges, boolean directed) {
+
+		for (int i = 0; i < edges.size(); i++) {
 
 			int source = edges.get(i).source;
 			int dest = edges.get(i).dest;
@@ -25,10 +36,32 @@ public class Graph {
 			// Add an edge from src to dest
 			adjList.get(source).add(dest);
 
-			// Since graph is undirected, add an edge from dest to src also 
-			adjList.get(dest).add(source);
+			// Since graph is undirected, add an edge from dest to src also
+			if (!directed) {
+				adjList.get(dest).add(source);
+			}
 
 		}
+	}
 
+	public void addEdge(Edge edge, boolean directed) {
+		// Add an edge from src to dest
+		int source = edge.source;
+		int dest = edge.dest;
+
+		adjList.get(source).add(dest);
+		// Since graph is undirected, add an edge from dest to src also
+		if (!directed) {
+			adjList.get(dest).add(source);
+		}
+	}
+
+	public void createEpmtyEdges(int N) {
+
+		adjList = new ArrayList<>(N);
+
+		for (int i = 0; i < N; i++) {
+			adjList.add(i, new LinkedList<Integer>());
+		}
 	}
 }
