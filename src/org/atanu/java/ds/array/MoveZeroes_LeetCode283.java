fa03d6b63b9d5182be3375004bf68a0f3af2e220
Atanu <atanu2destiny@gmail.com>
2021-01-13 21:41:53 +0530

    move Zeroes . LeetCode 283

---
+++ src/org/atanu/java/ds/array/MoveZeroes_LeetCode283.java
@@ -0,0 +1,35 @@
+package org.atanu.java.ds.array;
+
+import java.util.Arrays;
+
+public class MoveZeroes_LeetCode283{
+
+    public static void moveZeroes(int[] nums) {
+        int pivot = 0;
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            // Just Change this condition To move Last or First
+            // == is for first as . will copy the zero to first and move on
+            if(nums[i] != pivot){ 
+                swap(nums, i, index);
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int[] nums, int i , int j){
+        
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] nums = new int[] {0,1,0,3,12};
+        moveZeroes(nums);
+
+    }
+}
