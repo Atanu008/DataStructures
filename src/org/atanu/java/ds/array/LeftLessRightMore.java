2c5299cda3f0bb59cd37d79a7950d2373ea2c557
Atanu <atanu2destiny@gmail.com>
2021-03-11 18:26:11 +0530

    new file

---
+++ src/org/atanu/java/ds/array/LeftLessRightMore.java
@@ -0,0 +1,11 @@
+package org.atanu.java.ds.array;
+
+public class LeftLessRightMore {
+    
+    public int findElement(int[] nums){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
