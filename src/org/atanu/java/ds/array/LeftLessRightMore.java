aeff11ab1af43a45a57c92c44d81e2edb288d4b7
Atanu <atanu2destiny@gmail.com>
2021-03-20 19:42:53 +0530

    return added

--- src/org/atanu/java/ds/array/LeftLessRightMore.java
+++ src/org/atanu/java/ds/array/LeftLessRightMore.java
@@ -3,7 +3,7 @@ package org.atanu.java.ds.array;
 public class LeftLessRightMore {
     
     public int findElement(int[] nums){
-
+        return 0;
     }
     public static void main(String[] args) {
         
