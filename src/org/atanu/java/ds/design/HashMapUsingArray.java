263be13a0dd4e0f2fa7006f5798cf04fe191adfc
Atanu <atanu2destiny@gmail.com>
2021-03-27 13:24:51 +0530

    HashMap using array

---
+++ src/org/atanu/java/ds/design/HashMapUsingArray.java
@@ -0,0 +1,52 @@
+package org.atanu.java.ds.design;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+//https://leetcode.com/problems/design-hashmap/
+public class HashMapUsingArray {
+    int [] map;
+    /** Initialize your data structure here. */
+    public HashMapUsingArray() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return map[key];
+
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        map[key] = -1;
+    }
+
+    public static void main(String[] args) {
+        HashMapUsingArray myHashMap = new HashMapUsingArray();
+        myHashMap.put(1, 1); // The map is now [[1,1]]
+        myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+        System.out.println(myHashMap.get(1));    // return 1, The map is now [[1,1], [2,2]]
+        System.out.println(myHashMap.get(2));    // return 2
+        System.out.println(myHashMap.get(3));    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+        myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+        System.out.println(myHashMap.get(2));    // return 1, The map is now [[1,1], [2,1]]
+        myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+        System.out.println(myHashMap.get(2));    // return -1 (i.e., not found), The map is now [[1,1]]
+    }
+}
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
