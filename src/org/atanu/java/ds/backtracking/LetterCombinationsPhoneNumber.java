d0e42bbbb6ee9384aefd3b0c9ca89ec8b7e9203e
Atanu <atanu2destiny@gmail.com>
2021-05-29 00:38:43 +0530

    BackTracking

---
+++ src/org/atanu/java/ds/backtracking/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,46 @@
+package org.atanu.java.ds.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+//Leetcode 17
+public class LetterCombinationsPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+
+        String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        List<String> result = new ArrayList<>();
+
+        if(digits == null || digits.length() == 0){
+            return result;
+        }
+
+        dfs(digits,0,"",map,result);
+
+        return result;
+
+    }
+
+    public void dfs(String digits, int index, String prefix, String[] map, List<String> result){
+
+        if(index >= digits.length()){
+            result.add(prefix);
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for(int i = 0; i < letters.length(); i++){
+
+            dfs(digits,index+1,prefix + letters.charAt(i),map,result);
+        }
+    }
+
+    public static void main(String[] args) {
+        LetterCombinationsPhoneNumber combinationsPhoneNumber = new LetterCombinationsPhoneNumber();
+        String number = "23";
+        List<String> result = combinationsPhoneNumber.letterCombinations(number);
+        System.out.println(result);
+
+    }
+}
