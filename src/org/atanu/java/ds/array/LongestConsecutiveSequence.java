7a84080283697eeb6f58912b1457d6605d83714e
Atanu <atanu2destiny@gmail.com>
2021-09-19 20:05:18 +0530

    LongestConsecutiveSeq

---
+++ src/org/atanu/java/ds/array/LongestConsecutiveSequence.java
@@ -0,0 +1,70 @@
+package org.atanu.java.ds.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    //Time complexity : O(nlgn). // For Sorting. main loop does t in O(n)
+    //Space complexity : O(1)O(1) (or O(n)O(n)).
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+
+        int currentStreak = 1;
+        int longestStreak = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    currentStreak += 1;
+                } else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
+            }
+        }
+        return longestStreak;
+
+    }
+
+    //Time complexity : O(n).
+    //Space complexity : O(1)O(n)
+    public int longestConsecutiveV2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        int longestSteak = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNumber = nums[i];
+            int currentSteak = 1;
+            // This if will make sure that we will only check from smallest number
+            if (!set.contains(currentNumber - 1)) {
+                while (set.contains(currentNumber + 1)) {
+                    currentNumber += 1;
+                    currentSteak += 1;
+                }
+            }
+
+            longestSteak = Math.max(longestSteak, currentSteak);
+        }
+
+        return longestSteak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100,4,200,1,3,2};
+        int longestCommonSequence = new LongestConsecutiveSequence().longestConsecutive(nums);
+        System.out.println("Longest Consecutive Sequence is "+ longestCommonSequence);
+
+        longestCommonSequence = new LongestConsecutiveSequence().longestConsecutiveV2(nums);
+        System.out.println("Longest Consecutive Sequence is "+ longestCommonSequence);
+    }
+}
---
+++ src/org/atanu/java/ds/binarytree/LongestConsecutiveSequenceInBinaryTree.java
@@ -0,0 +1,51 @@
+package org.atanu.java.ds.binarytree;
+
+//https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
+//https://www.geeksforgeeks.org/longest-consecutive-sequence-binary-tree/
+
+//if current node has value one more than its parent node then it makes a consecutive path,
+//At each node we will compare node’s value with its parent value and update the longest consecutive path accordingly.
+public class LongestConsecutiveSequenceInBinaryTree {
+
+    public int longestConsecutive(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int[] result = new int[1]; // AtomicInteger or any object can be used to pass in the recursion tree
+        longestConsecutive(root, root.data, 1, result);
+        return result[0];
+    }
+
+    public void longestConsecutive(TreeNode root, int expectedVal, int currentLength, int[] result){
+
+        if(root == null){
+            return;
+        }
+
+        if(root.data == expectedVal){
+            currentLength++;
+        }
+        else{
+            currentLength = 1;
+        }
+
+        result[0] = Math.max(result[0], currentLength);
+        longestConsecutive(root.left, root.data+1, currentLength, result);
+        longestConsecutive(root.right, root.data+1, currentLength, result);
+
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(6);
+        root.right = new TreeNode(9);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(10);
+        root.right.right.right = new TreeNode(11);
+        root.right.right.right.left = new TreeNode(12);
+        int longestCommonSequence = new LongestConsecutiveSequenceInBinaryTree().longestConsecutive(root);
+        System.out.println("Longest Consecutive Sequence is "+ longestCommonSequence);
+
+    }
+}
